7f8fec45d7853c1d14454783d081ed24a1d2b625
VyacheslavTretyak <tretyak1c@gmail.com>
2018-09-23 20:27:08 +0300

    create project

--- KeyLoger/KeyLogerApp.cs
+++ KeyLoger/KeyLogerApp.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace KeyLoger
 {
-	public partial class Form1 : Form
+	public partial class KeyLogerApp : Form
 	{
-		public Form1()
+		public KeyLogerApp()
 		{
 			InitializeComponent();
 		}
